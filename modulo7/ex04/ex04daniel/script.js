c57c8c8441159e79c9afcd967948c56c790740cb
Daniel Rodrigues da Silva <115389640+danielrs81@users.noreply.github.com>
2022-11-21 16:05:22 -0300

    up

---
+++ modulo7/ex04/ex04daniel/script.js
@@ -0,0 +1,22 @@
+let nome = document.getElementById('inome')
+let velocidade = document.getElementById('ivelocidade')
+
+function calcular (){
+    
+    if(velocidade <= 0) {
+        res.innerHTML(`A nave está parada, considere partir e aumentar a velocidade`)
+    }
+}
+
+
+
+if (velocity <= 0) {
+    alert('A nave está parada, considere partir e aumentar a velocidade')
+} else if (velocity < 40) {
+    alert('Você está devagar. Podemos aumentar a velocidade')
+} else if (velocity < 80) {
+    alert('Parece uma boa velocidade para manter')
+} else if (velocity < 100) {
+    alert('Velocidade alta, considere diminuir')
+} else {
+    alert('Velicidade de risco, Controle automático forçado')
--- modulo7/ex04/index.js
+++ modulo7/ex04/index.js
@@ -3,25 +3,21 @@ let velocity = 0
 let newVelocity = prompt('A que velocidae você gostaria de acelerar?')
 let confirmVelocity = confirm(`Estamos acelerando para ${newVelocity} km/s`)
 
-if(confirmVelocity) {
+if (confirmVelocity) {
     velocity = newVelocity
-} 
-
-if(velocity <= 0) {
-    alert('A nave está parada, considere partir e aumentar a velocidade')
-} else {
-    if(velocity < 40) {
-        alert('Você está devagar. Podemos aumentar a velocidade')
-    } else {
-        if(velocity < 80) {
-            alert('Parece uma boa velocidade para manter')
-        } else {
-            if(velocity <100) {
-                alert('Velocidade alta, considere diminuir')
-            } else {
-                alert('Velicidade de risco, Controle automático forçado')
-            }
-        }
-    }
 }
+
+if (velocity <= 0) {
+    alert('A nave está parada, considere partir e aumentar a velocidade')
+} else if (velocity < 40) {
+    alert('Você está devagar. Podemos aumentar a velocidade')
+} else if (velocity < 80) {
+    alert('Parece uma boa velocidade para manter')
+} else if (velocity < 100) {
+    alert('Velocidade alta, considere diminuir')
+} else {
+    alert('Velicidade de risco, Controle automático forçado')
+}
+
+alert(`Piloto: ${pilotName}\nVelocidade: ${velocity} km's`)
 
