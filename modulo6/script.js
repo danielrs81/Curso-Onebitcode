13e4d0fc8d429f7fead96eef168e6d2a52bbb3fe
Daniel Rodrigues da Silva <115389640+danielrs81@users.noreply.github.com>
2022-11-21 15:41:49 -0300

    up

---
+++ modulo6/script.js
@@ -0,0 +1,11 @@
+let nomeVelha = prompt('Qual o nome da pessoa mais velha?')
+let idadeVelha = prompt('Qual a idade da pessoa mais velha?')
+let nomeNova = prompt('Qual o nome da pessoa mais nova?')
+let idadeNova = prompt('Qual a idade da pessoa mais nova?')
+
+let dif = idadeVelha - idadeNova
+
+window.alert(`A pessoa mais velha é ${nomeVelha}\n que tem ${idadeVelha} anos, \ne a mais nova é a pessoa com o nome ${nomeNova} com ${idadeNova} anos, \n \n a diferença de idade entre os dois seria de\n ${dif} anos`)
+
+
+
