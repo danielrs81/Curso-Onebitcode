89ac435eed5cdafcbf604495d7b26cedf5f4a844
Daniel Rodrigues da Silva <115389640+danielrs81@users.noreply.github.com>
2022-11-23 17:56:36 -0300

    up

--- modulo7/exAula87Daniel/script.js
+++ modulo7/exAula87Daniel/script.js
@@ -1,12 +1,12 @@
 function calcular() {
     let dataPartida = document.getElementById('idata')
     let dataDigitada = moment(dataPartida, "DD/MM/YYYY") //Converter de String para o formatdo de data usando o 'Moment'
-    
     let today = moment()
 
     let dataDiferenca = today - dataDigitada
     
     let res = document.getElementById('res')
+    
     if(dataDigitada == 0) {
         res.innerHTML = `Data em branco`
     }
